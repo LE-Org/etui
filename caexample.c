622ffb4dc42312796eba175ebf36375307859fa5
hir12111 <hir12111@gmail.com>
2020-03-03 20:30:39 +0000

    Pull out example of using CA helper functions

---
+++ caexample.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "cahelper.h"
+
+// Example of using CA helper functions
+
+void
+callback(struct sub_info *sub)
+{
+	if (sub->dbr_type == DBR_GR_DOUBLE) {
+		struct dbr_gr_double *pv = (struct dbr_gr_double *) sub->value;
+		printf("Got type: %ld value: %f\n", sub->dbr_type, pv->value);
+	}
+}
+
+int
+main()
+{
+	struct sub_info sub;
+	start_ca();
+	init_sub_info(&sub, "user:ai1", callback, NULL);
+	subscribe_pv(&sub);
+	while (1) {
+		process_ca_events();
+	}
+	unsubscribe_pv(&sub);
+	stop_ca();
+	return 0;
+}
+
