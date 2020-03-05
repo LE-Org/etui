09b25594cb177fe96bb88c5d93df669b8a3c9e4f
hir12111 <hir12111@gmail.com>
2020-03-05 22:03:57 +0000

    Handle borders correctly when resizing

--- src/tui.c
+++ src/tui.c
@@ -23,7 +23,7 @@
 static int wcmds_search(int);
 static void recreate_menu(void);
 static void recreate_items_from_pvs(void);
-static void border_if_active(WINDOW *);
+static void set_borders();
 static void release(void);
 int start_tui(void);
 int stop_tui(void);
@@ -39,10 +39,19 @@ int process_tui_events(void);
 
 static int npvs = 0;
 static struct graphical_pv *gpvs[MAX_N_ENTRIES + 1];
-static WINDOW *win_menu, *win_flds, *win_main, *win_stat, *win_cmds;
+
+#define N_WINDOWS (5)
+enum {
+	WIN_MENU=0,
+	WIN_FLDS,
+	WIN_MAIN,
+	WIN_STAT,
+	WIN_CMDS
+} active_win;
+
+static WINDOW *win[N_WINDOWS];
 static MENU *menu;
 static ITEM *mitems[MAX_N_ENTRIES + 1];
-static WINDOW *active_win;
 static unsigned tui_flags = 0;
 
 // windows dimensions
@@ -56,8 +65,8 @@ wcmds_search(int c)
 {
 	if (c == '\n') {
 		/* hide search field */
-		wmove(win_cmds, 0, 0);
-		wclrtoeol(win_cmds);
+		wmove(win[WIN_CMDS], 0, 0);
+		wclrtoeol(win[WIN_CMDS]);
 
 		return 1;
 	}
@@ -68,9 +77,9 @@ wcmds_search(int c)
 		menu_driver(menu, c); /* TODO: check c */
 
 	/* update search field, but leave '/' character alone */
-	wmove(win_cmds, 0, 1);
-	wclrtoeol(win_cmds);
-	mvwaddstr(win_cmds, 0, 1, menu_pattern(menu));
+	wmove(win[WIN_CMDS], 0, 1);
+	wclrtoeol(win[WIN_CMDS]);
+	mvwaddstr(win[WIN_CMDS], 0, 1, menu_pattern(menu));
 
 	return 0;
 }
@@ -86,39 +95,39 @@ recreate_windows()
 	wflds_w = maxx * WFLDS_FRAC / TOTAL_FRAC;
 	wmain_w = maxx - wmenu_w - wflds_w;
 	wstat_w = wcmd_w = maxx;
-	if (win_menu) {
+	if (win[WIN_MENU]) {
 		unpost_menu(menu);
 		free_menu(menu);
 		menu = NULL;
-		delwin(win_menu);
-		delwin(win_flds);
-		delwin(win_main);
-		delwin(win_cmds);
+		delwin(win[WIN_MAIN]);
+		delwin(win[WIN_FLDS]);
+		delwin(win[WIN_MAIN]);
+		delwin(win[WIN_STAT]);
+		delwin(win[WIN_CMDS]);
 	}
 	/* define window dimensions */
-	win_menu = newwin(wmenu_h, wmenu_w, 0, 0);
-	win_flds = newwin(wflds_h, wflds_w, 0, wmenu_w);
-	win_main = newwin(wmain_h, wmain_w, 0, wmenu_w + wflds_w);
-	win_stat = newwin(WSTAT_LINES, wstat_w, wmenu_h, 0);
-	win_cmds = newwin(WCMDS_LINES, wcmd_w, wmenu_h + WSTAT_LINES, 0);
+	win[WIN_MENU] = newwin(wmenu_h, wmenu_w, 0, 0);
+	win[WIN_FLDS] = newwin(wflds_h, wflds_w, 0, wmenu_w);
+	win[WIN_MAIN] = newwin(wmain_h, wmain_w, 0, wmenu_w + wflds_w);
+	win[WIN_STAT] = newwin(WSTAT_LINES, wstat_w, wmenu_h, 0);
+	win[WIN_CMDS] = newwin(WCMDS_LINES, wcmd_w, wmenu_h + WSTAT_LINES, 0);
 
 	/* color windows */
 	if (tui_flags & TUI_HAS_COLORS) {
 		/* win_stat */
-		wbkgd(win_stat, COLOR_PAIR(1));
+		wbkgd(win[WIN_STAT], COLOR_PAIR(1));
 	} else {
 		int y, x;
 
 		/* win_stat */
 		/* fake wbkgd by filling spaces in reverse video */
-		wattr_on(win_stat, A_REVERSE, NULL);
+		wattr_on(win[WIN_STAT], A_REVERSE, NULL);
 		for (y = 0; y < WSTAT_LINES; y++) {
-			wmove(win_stat, y, 0);
+			wmove(win[WIN_STAT], y, 0);
 			for (x = 0; x < wstat_w; x++)
-				waddstr(win_stat, " ");
+				waddstr(win[WIN_STAT], " ");
 		}
 	}
-	active_win = win_menu;
 }
 
 static void
@@ -134,8 +143,9 @@ recreate_menu(void)
 	}
 	recreate_items_from_pvs();
 	menu = new_menu((ITEM **) mitems);
-	set_menu_win(menu, win_menu);
-	set_menu_sub(menu, derwin(win_menu, menu_h, menu_w, 1, 1));
+	set_menu_win(menu, win[WIN_MENU]);
+	/* TODO: fix this memory leak */
+	set_menu_sub(menu, derwin(win[WIN_MENU], menu_h, menu_w, 1, 1));
 	set_menu_format(menu, menu_h, 1);
 	set_menu_mark(menu, "-");
 	post_menu(menu);
@@ -167,12 +177,15 @@ recreate_items_from_pvs(void)
 
 
 static void
-border_if_active(WINDOW *w)
+set_borders()
 {
-	if (w == active_win)
-		box(w, 0, 0);
-	else
-		wborder(w,' ',' ',' ',' ',' ',' ',' ',' ');
+	int i;
+	for (i=0; i < N_WINDOWS; i++) {
+		if (active_win == i)
+			box(win[i], 0, 0);
+		else
+			wborder(win[i],' ',' ',' ',' ',' ',' ',' ',' ');
+	}
 }
 
 static void
@@ -202,6 +215,7 @@ start_tui(void)
 	/* create panels */
 	recreate_windows();
 	recreate_menu();
+	active_win = WIN_MENU;
 
 	return 0;
 }
@@ -235,7 +249,7 @@ create_tui_entry(const char *name)
 
 	/* create items */
 	refresh_menu_items();
-	wrefresh(win_menu);
+	wrefresh(win[WIN_MENU]);
 	return entry_id;
 }
 
@@ -250,8 +264,7 @@ update_tui_entry(int entry_id, const char *value)
 static void
 draw_win_menu(void)
 {
-	border_if_active(win_menu);
-	wrefresh(win_menu);
+	wrefresh(win[WIN_MENU]);
 }
 
 static void
@@ -263,8 +276,8 @@ draw_win_flds(void)
 		return;
 
 	for (i = 0; i < menu_h && (i+top_row(menu)) < npvs; ++i)
-		mvwprintw(win_flds,i+1,1,"%s",gpvs[i+top_row(menu)]->value);
-	wrefresh(win_flds);
+		mvwprintw(win[WIN_FLDS],i+1,1,"%s",gpvs[i+top_row(menu)]->value);
+	wrefresh(win[WIN_FLDS]);
 }
 
 static void
@@ -273,29 +286,28 @@ draw_win_main(void)
 	if (top_row(menu) == -1)
 		return;
 
-	wmove(win_main, 1, 1); wclrtoeol(win_main);
-	mvwaddstr(win_main, 1, 1, item_name(current_item(menu)));
-	wmove(win_main, 2, 1); wclrtoeol(win_main);
-	mvwprintw(win_main, 2, 1, "%d.",
+	wmove(win[WIN_MAIN], 1, 1); wclrtoeol(win[WIN_MAIN]);
+	mvwaddstr(win[WIN_MAIN], 1, 1, item_name(current_item(menu)));
+	wmove(win[WIN_MAIN], 2, 1); wclrtoeol(win[WIN_MAIN]);
+	mvwprintw(win[WIN_MAIN], 2, 1, "%d.",
 		  item_index(current_item(menu)));
-	wmove(win_main, 3, 1); wclrtoeol(win_main);
-	mvwprintw(win_main, 3, 1, "VAL = %s",
+	wmove(win[WIN_MAIN], 3, 1); wclrtoeol(win[WIN_MAIN]);
+	mvwprintw(win[WIN_MAIN], 3, 1, "VAL = %s",
 		  gpvs[item_index(current_item(menu))]->value);
-	border_if_active(win_main);
-	wrefresh(win_main);
+	wrefresh(win[WIN_MAIN]);
 }
 
 static void
 draw_win_stat(void)
 {
-	mvwprintw(win_stat,0,1,"%d PVs", npvs);
-	wrefresh(win_stat);
+	mvwprintw(win[WIN_STAT],0,1,"%d PVs", npvs);
+	wrefresh(win[WIN_STAT]);
 }
 
 static void
 draw_win_cmds(void)
 {
-	wrefresh(win_cmds);
+	wrefresh(win[WIN_CMDS]);
 }
 
 static void
@@ -314,15 +326,20 @@ process_tui_events(void)
 	int c;
 
 	c = getch();
+	if (c == KEY_RESIZE) {
+		recreate_windows();
+		recreate_menu();
+		return 0;
+	}
 	if (c == 'q') /* quit condition */
 		return 1;
-	if (active_win == win_cmds) {
+	if (active_win == WIN_CMDS) {
 		if (wcmds_search(c)) { /* finish */
-			wmove(win_cmds, 0, 0); wclrtoeol(win_cmds);
-			active_win = win_menu;
+			wmove(win[WIN_CMDS], 0, 0); wclrtoeol(win[WIN_CMDS]);
+			active_win = WIN_MENU;
 		}
 	}
-	if (active_win == win_menu) {
+	if (active_win == WIN_MENU) {
 		switch (c) {
 		/* menu movement */
 		case KEY_DOWN: case 'j':
@@ -351,30 +368,27 @@ process_tui_events(void)
 		case 'p':
 			menu_driver(menu, REQ_PREV_MATCH);
 			break;
-		case KEY_RESIZE:
-			recreate_windows();
-			recreate_menu();
-			break;
 		}
 	}
 
 	switch (c) {
 	/* select active win */
 	case '\t':
-		active_win = (active_win == win_menu) ?
-			     win_main : win_menu;
+		active_win = (active_win == WIN_MENU) ?
+			     WIN_MAIN : WIN_MENU;
 		break;
 	/* search mode */
 	case '/':
-		mvwaddch(win_cmds, 0, 0, '/');
-		active_win = win_cmds;
+		mvwaddch(win[WIN_CMDS], 0, 0, '/');
+		active_win = WIN_CMDS;
 		break;
 	/* command mode */
 	case ':':
-		mvwaddch(win_cmds, 0, 0, ':');
+		mvwaddch(win[WIN_CMDS], 0, 0, ':');
 		break;
 	}
 
+	set_borders();
 	draw_windows();
 
 	return 0;
